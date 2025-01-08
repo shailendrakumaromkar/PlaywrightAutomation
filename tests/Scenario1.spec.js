401138feddca6fb429680da062e8d009752a6196
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2025-01-09 02:44:36 +0530

    start with locator

--- tests/Scenario1.spec.js
+++ tests/Scenario1.spec.js
@@ -5,8 +5,8 @@ test("First Test", async({page})=>{
  //await  page.goto("https://playwright.dev/");
  await  page.goto("https://www.coingecko.com/en/categories/meme-token");
 
- await expect (page).toHaveTitle(/Playwright/)
-
+// await expect (page).toHaveTitle(/Playwright/)
+await page.locator("")
  //await page.locator(".getStarted_Sjon").click();
 
  //await page.getByText("Get started").click();
